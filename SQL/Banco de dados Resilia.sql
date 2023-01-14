d2620163138c30622578a3ac9186f73e19251391
Paula Dinucci <paulaldinucci@gmail.com>
2023-01-14 00:51:04 -0300

    Script diagrama
    scripts do diagrama do exercício do módulo 4

--- SQL/Banco de dados Resilia.sql
+++ SQL/Banco de dados Resilia.sql
@@ -1,31 +1,24 @@
 CREATE TABLE `Curso` (
-  `Nome_curso` varchar(255),
+  `nome_curso` varchar(255),
   `Turno` varchar(255),
-  `Formato_curso` varchar(255)
+  `formato_curso` varchar(255),
+  `data_inicio` int,
+  `data_termino` int
 );
 
 CREATE TABLE `Turmas` (
+  `Professores` varchar(255),
   `Turno` varchar(255),
-  `Alunos` varchar(255),
-  `Professores` varchar(255)
+  `Alunos` varchar(255)
 );
 
 CREATE TABLE `Alunos` (
   `id` int PRIMARY KEY,
-  `Nome` varchar(255),
-  `Data_nascimento` int,
-  `Endereco` varchar(255)
+  `nome` varchar(255),
+  `data_nascimento` int,
+  `endereco` varchar(255)
 );
 
-CREATE TABLE `Turmas_Curso` (
-  `Turmas_Turno` varchar,
-  `Curso_Turno` varchar,
-  PRIMARY KEY (`Turmas_Turno`, `Curso_Turno`)
-);
-
-ALTER TABLE `Turmas_Curso` ADD FOREIGN KEY (`Turmas_Turno`) REFERENCES `Turmas` (`Turno`);
-
-ALTER TABLE `Turmas_Curso` ADD FOREIGN KEY (`Curso_Turno`) REFERENCES `Curso` (`Turno`);
-
+ALTER TABLE `Turmas` ADD FOREIGN KEY (`Turno`) REFERENCES `Curso` (`Turno`);
 
 ALTER TABLE `Turmas` ADD FOREIGN KEY (`Alunos`) REFERENCES `Alunos` (`id`);
